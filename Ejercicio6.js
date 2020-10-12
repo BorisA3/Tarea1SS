dd7621d1673d1e839b88cba8a3bd94c56ffb8592
BorisA3 <botisalarcon@gmail.com>
2020-10-12 12:05:16 -0600

    Creacion de vistas y funciones de los ejercicios

---
+++ Ejercicio6.js
@@ -0,0 +1,15 @@
+exports.Ejercicio6 = (letrasMinusculas) => {
+
+    let palabra = letrasMinusculas;
+    let letras;
+       
+    console.log(palabra)
+      
+        function palabraEncontrada(encontrarPlabra) {
+            extraerLetras = encontrarPlabra.match(/[a-z]/g)
+            console.log(extraerLetras);
+        }
+            
+        palabraEncontrada(palabra);
+
+}
